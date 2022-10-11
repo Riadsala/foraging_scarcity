3e5c20d2d92991c2e023203d2920ea6c96f215ad
scienceanna <hughesannae@gmail.com>
2022-10-11 09:30:28 +0100

    running clarke 2020

---
+++ scripts/clarke/parse_clarke.R
@@ -0,0 +1,50 @@
+library(tidyverse)
+library(rstan)
+library(tidybayes)
+library(patchwork)
+library(scales)
+
+source("../../functions/sim_foraging_data.R")
+source("../../functions/prep_data.R")
+source("../../functions/get_run_info.R")
+
+
+options(mc.cores = parallel::detectCores())
+
+# chopping down a bit
+# 4 people, 5 trials per block
+
+dc <- read_csv("../../data/clarke2020/clarke_2020_qjep.csv") %>%
+  select(person = "observer", block = "condition", trial = "trial",  
+         id = "id", found = "found", class = "targ_type",
+         x = "x", y = "y") %>%
+  filter(person < 5,
+         trial < 6) 
+
+dc %>% mutate(x = as.vector(rescale(x, to = c(0.01, 0.99))),
+              y = as.vector(rescale(y, to = c(0.01, 0.99)))) -> dc 
+
+d_stim <- dc %>% select(person, block, trial, id, x, y, class) %>%
+  arrange(person, block, trial) 
+
+d_found <- dc %>% filter(found > 0) %>% 
+  arrange(person, block, trial, found) 
+
+
+d_found %>% filter(block == 1, trial == 2, person == 1) %>%
+  ggplot(aes(x, y)) + 
+  geom_label(aes(label = id, colour = as.factor(class))) + 
+  geom_path(data = d_found %>% filter(block == 1, trial == 2, person == 1, found>0),size = 1)
+
+d_list <- prep_data_for_stan(d_found, d_stim)
+
+#saveRDS(d_list, 'clarke_stan.rds')
+
+m <- stan("../../models/foraging_model_multilevel.stan", data = d_list, 
+          chains = 4, iter = 1000)
+summary(m)
+
+# plotting
+source("../../functions/plot_model.R")
+
+plot_model_fixed(m, dc)  
