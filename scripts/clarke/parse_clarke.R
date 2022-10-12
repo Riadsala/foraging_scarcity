70f70d54d3554492206a9cdafb4b5c91393d5391
scienceanna <hughesannae@gmail.com>
2022-10-12 15:14:26 +0100

    a bit of light noodling

--- scripts/clarke/parse_clarke.R
+++ scripts/clarke/parse_clarke.R
@@ -11,14 +11,16 @@ source("../../functions/get_run_info.R")
 
 options(mc.cores = parallel::detectCores())
 
-# it gets stuck if there are missing people
 # it also gets stuck if people have different numbers of trials
 
 dc <- read_csv("../../data/clarke2020/clarke_2020_qjep.csv") %>%
   select(person = "observer", block = "condition", trial = "trial",  
          id = "id", found = "found", class = "targ_type",
          x = "x", y = "y") %>%
-  filter(trial == 20) 
+  filter(trial < 6) %>%
+  filter(person < 6)
+
+write_csv(dc, 'subset_clarke.csv')
 
 dc %>% mutate(x = as.vector(rescale(x, to = c(0.01, 0.99))),
               y = as.vector(rescale(y, to = c(0.01, 0.99)))) -> dc 
@@ -41,11 +43,13 @@ d_found %>% filter(block == 1, trial == 2, person == 1) %>%
 
 d_list <- prep_data_for_stan(d_found, d_stim)
 
-#saveRDS(d_list, 'clarke_stan.rds')
+saveRDS(d_list, 'clarke_stan.rds')
 
 m <- stan("../../models/foraging_model_multilevel.stan", data = d_list, 
           chains = 4, iter = 1000)
 summary(m)
+
+#saveRDS(m, 'clarke_model.rds')
 
 # plotting
 source("../../functions/plot_model.R")
@@ -53,3 +57,4 @@ source("../../functions/plot_model.R")
 plot_model_fixed(m, dc)
 plot_model_spatial(m, dc)
 plot_init_sel(m, dc)
+
