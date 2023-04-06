b6919123de8b8abc94010e4ade5e1c5c4dcd13e8
Alasdair Clarke <a.clarke@essex.ac.uk>
2023-04-06 12:48:43 +0100

    Update parsing_polygon_pilot_exp_data.R

--- scripts/parsing_polygon_pilot_exp_data.R
+++ scripts/parsing_polygon_pilot_exp_data.R
@@ -67,10 +67,12 @@ d_list$prior_mu_phidis <- 10
 
 #d_list$targ_class <- d_list$targ_class-1
 
-m <- stan("../../foraging_spatial/models/foraging_model1.stan", data = d_list, 
-          chains = 1, iter = 1000)
+ m <- stan("../../foraging_spatial/models/foraging_model1.stan", data = d_list,
+        chains = 1, iter = 1000)
 
-saveRDS(m, "foraging_pilot.model")
+ saveRDS(m, "foraging_pilot.model")
+
+
 
 
 blks_labels <- levels(d_found$condition)
